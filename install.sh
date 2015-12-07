32d4a844928c78e4c88ea6632e91a5825a85b986
nadayar <nenegesi@gmail.com>
2015-12-07 16:09:41 +0300

    fixed Andela blue formatting and evals

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo -e "\e[34m installing Andela Crunchies Auto-Nominater...."
+echo -e "\e[34m installing Andela Crunchies Auto-Nominater.... \e[0m"
 
 function go_to_downloads_directory {
-  echo -e "\e[34m change directory to Downloads"
+  echo -e "\e[34m change directory to Downloads \e[0m"
   cd ~/Downloads
 }
 
 function clone_crunchies {
-  echo -e "\e[34m cloning crunchies repo...."
+  echo -e "\e[34m cloning crunchies repo.... \e[0m"
   git clone https://github.com/andela/crunchies.git 
   cd crunchies
 }
 
 function assert_user_has_homebrew {
-  echo -e "\e[34m assert that user has homebrew installed"
+  echo -e "\e[34m assert that user has homebrew installed \e[0m"
   BREW=`which brew`
   if [[ "$BREW" != "/usr/local/bin/brew" ]]; then
     # User does not have brew installed
@@ -23,7 +23,7 @@ function assert_user_has_homebrew {
 }
 
 function give_el_capitan_users_the_good_python_give_older_os_users_pip {
-  echo -e "\e[34m make sure El Capitan users have a stable version of python and pip"
+  echo -e "\e[34m make sure El Capitan users have a stable version of python and pip \e[0m"
   if [[ "$OSTYPE" == "darwin15"* ]]; then
     # El Capitan
     brew reinstall python
@@ -35,43 +35,43 @@ function give_el_capitan_users_the_good_python_give_older_os_users_pip {
 }
 
 function install_chromedriver {
-  echo -e "\e[34m install chromedriver"
+  echo -e "\e[34m install chromedriver \e[0m"
   brew install chromedriver
 }
 
 function globally_install_python_requirements {
-  echo -e "\e[34m install python script requirements and dependencies"
+  echo -e "\e[34m install python script requirements and dependencies \e[0m"
   sudo pip install -r requirements.txt
 }
 
 function copy_app_and_launcher_config {
-  echo -e "\e[34m copy app to Applications and set launch config"
+  echo -e "\e[34m copy app to Applications and set launch config \e[0m"
   sudo cp -vr ~/Downloads/crunchies/nominate.app /Applications/nominate.app
   sudo cp -av ~/Downloads/crunchies/nominate.plist /Library/LaunchAgents/nominate.plist
 }
 
 function launch_scheduler {
-  echo -e "\e[34m launch scheduler config"
+  echo -e "\e[34m launch scheduler config \e[0m"
   launchctl load /Library/LaunchAgents/nominate.plist
 }
 
 function clean_up {
-  echo -e "\e[34m clean up"
-  CRUNCHIES_DIRECTORY="~/Downloads/crunchies"
-  if [ eval -d "$CRUNCHIES_DIRECTORY" ]; then
+  echo -e "\e[34m clean up \e[0m"
+  eval CRUNCHIES_DIRECTORY="~/Downloads/crunchies"
+  if [ -d "$CRUNCHIES_DIRECTORY" ]; then
     # Control will enter here if $DIRECTORY exists.
-    echo -e "\e[34m deleting already existing crunchies directory"
-    eval sudo rm -rf "$CRUNCHIES_DIRECTORY"
+    echo -e "\e[34m deleting already existing crunchies directory \e[0m"
+    sudo rm -rf "$CRUNCHIES_DIRECTORY"
   fi
 }
 
 function display_completion_notification {
-  echo -e "\e[34m display notification"
+  echo -e "\e[34m display notification \e[0m"
   osascript -e 'display notification "Crunchies Installation complete!" with title "Andela Crunchies"'
 }
 
 function close_terminal {
-  echo -e "\e[34m close terminal window"
+  echo -e "\e[34m close terminal window \e[0m"
   osascript -e 'tell application "Terminal" to close first window' & exit
 }
 
